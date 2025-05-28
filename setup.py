6d0d9fd6ea42a0a4e5d2566ba815838a4268f9ce
Aleksandr Popov <aleneus@bk.ru>
2025-05-28 11:45:16 +0300

    docs: update license

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2021-2025 Aleksandr Popov
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the Lesser GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -10,10 +12,8 @@
 
 # You should have received a copy of the Lesser GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from setuptools import setup
 from pyqtest import __version__
-
 
 setup(
     name="pyqtest",
